70177db98c1bb5a36a93a5c880605deda30830d9
Foroozan Iraji <forooz.iraji@gmail.com>
2026-02-25 09:56:49 +0330

    Initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+// ---------- layout ----------
+export { Navbar } from "./layout/Navbar";
+export { Footer } from "./layout/Footer";
+export { ScrollToTop } from "./ui/custom/buttons/ScrollToTop";
+
+// ---------- sections/landing ----------
+export { Hero } from "./sections/landing/Hero";
+export { Features } from "./sections/landing/Features";
+
+// ---------- sections/blog ----------
+export { BlogLayout } from "./sections/blog/BlogLayout";
+export { BlogCMS } from "./sections/blog/BlogCMS";
+export { BlogList } from "./sections/blog/BlogList";
+export { BlogPostCard } from "./sections/blog/BlogPostCard";
+export { BlogPostForm } from "./sections/blog/BlogPostForm";
+export { BlogPost } from "./sections/blog/BlogPost";
